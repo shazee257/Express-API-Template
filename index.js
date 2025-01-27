ab9e32c5532e79c1366def7771965abec0fcad02
Shahzad <shazee257@gmail.com>
2025-01-27 19:29:36 +0500

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cors({ origin: "*", credentials: true }));
 
-app.get('/', (req, res) => generateResponse(null, `${process.env.APP_NAME} API - Health check passed`, res));
+app.get('/', (req, res) => generateResponse(null, `${process.env.APP_NAME} API v1.0 - Health check passed`, res));
 
 const appRoutes = new API(app);
 appRoutes.registerGroups();
